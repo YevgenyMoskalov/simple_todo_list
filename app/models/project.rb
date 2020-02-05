115618dcb94a2fdc05a40ee253c11c417f131449
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-05 21:11:09 +0200

    add bootstrap (sign_in and sign_up)

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+  belongs_to :user
   has_many :users
   validates :name, presence: true, length: { maximum: 255,
                                              too_long: '%{count} characters is the maximum allowed' }
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
+  belongs_to :project
   has_many :projects
   validates :description, :status, presence: true, length: { maximum: 255,
                                                             too_long: '%{count} characters is the maximum allowed' }
