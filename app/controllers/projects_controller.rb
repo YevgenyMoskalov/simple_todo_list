379e9ffd95fd7b430b5cf9e4601ae800574cd3c5
YevgenyMoskalov <lesnoy022@gmail.com>
2020-02-10 17:41:49 +0200

    update index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,4 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.where(user_id: current_user.id).all
   end
-
-  def show
-    @tasks = Task.where(project_id: project_url).all
-  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,3 @@
 class TasksController < ApplicationController
-  def index
-    @tasks = Task.where(project_id: @project.id).all
-  end
 
-  # def show
-  #   @task = Task.where(project_id: @project.id)[:id]
-  # end
 end
